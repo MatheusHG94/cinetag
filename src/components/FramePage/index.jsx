c7729d9796cef744237d48762d645cb93cebe9e7
Matheus <mat.hen.gra@gmail.com>
2023-07-19 12:19:44 -0300

    nested routes

--- src/components/FramePage/index.jsx
+++ src/components/FramePage/index.jsx
@@ -3,14 +3,17 @@ import styles from './FramePage.module.css';
 import { Outlet } from 'react-router-dom';
 import Header from 'components/Header';
 import Footer from 'components/Footer';
+import { FavoritesProvider } from 'context/Favorites';
 
 export default function FramePage() {
   return (
     <>
       <Header />
-      <section className={styles.container}>
-        <Outlet />
-      </section>
+      <FavoritesProvider>
+        <section className={styles.container}>
+          <Outlet />
+        </section>
+      </FavoritesProvider>
       <Footer />
     </>
   )
--- src/routes.jsx
+++ src/routes.jsx
@@ -3,23 +3,20 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Main from 'pages/Main';
 import Favorites from 'pages/Favorites';
 import FramePage from 'components/FramePage';
-import { FavoritesProvider } from 'context/Favorites';
 import Player from 'pages/Player';
 import NotFound from 'pages/NotFound';
 
 export default function AppRoutes() {
     return (
         <BrowserRouter>
-            <FavoritesProvider>
-                <Routes>
-                    <Route path='/' element={<FramePage />}>
-                        <Route path='/' element={<Main />} />
-                        <Route path='/favorites' element={<Favorites />} />
-                        <Route path='/:id' element={<Player />} />
-                        <Route path='*' element={<NotFound />} />
-                    </Route>
-                </Routes>
-            </FavoritesProvider>
+            <Routes>
+                <Route path='/' element={<FramePage />}>
+                    <Route index element={<Main />} />
+                    <Route path='favorites' element={<Favorites />} />
+                    <Route path=':id' element={<Player />} />
+                    <Route path='*' element={<NotFound />} />
+                </Route>
+            </Routes>
         </BrowserRouter>
     )
 }
